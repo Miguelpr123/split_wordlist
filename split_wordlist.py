58bc1299c8dedf58d62e637944eb988f9f345b0b
Miguelpr123 <miguelperezrodriguez335@gmail.com>
2023-08-17 16:13:43 -0400

    File Output

--- split_wordlist.py
+++ split_wordlist.py
@@ -8,24 +8,25 @@ import math
 
 # ------------- CONFIG ---------------
 num_files_to_divide = 3
-file_path = ''
-file_name = 'parameters' 
+file_path = '/home/<user>/Desktop' #or ''
+file_name = 'example'   #example.txt 
 file_extension = '.txt'
+file_output = '/home/<user>/Desktop' #or ''
 # --------------------------------------
 
 
-file = open( file_path + file_name + file_extension , 'rt' )
+file = open( file_path + '/' + file_name + file_extension , 'rt' )
 arr = file.readlines()
 num_lines_for_file = math.ceil( len(arr)/num_files_to_divide ) 
 file.close()
 
-bucle = 1
-while(bucle<=num_files_to_divide):
-    f = open( file_path + file_name + '_' + str(bucle) + file_extension , 'w' )
-    a = arr[ slice( (bucle-1)*num_lines_for_file , bucle*num_lines_for_file ) ]
+loop = 1
+while(loop<=num_files_to_divide):
+    f = open( file_output + '/' + file_name + '_' + str(loop) + file_extension , 'w' )
+    a = arr[ slice( (loop-1)*num_lines_for_file , loop*num_lines_for_file ) ]
     f.writelines( a )
     f.close()
-    bucle+=1
+    loop+=1
 
 
 
